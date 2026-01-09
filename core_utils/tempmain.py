94ab5427e7c3ca5ad1dfda6ab3873e4a812d7e4d
Lokesh Dulla <lokesh.dulla@interviewbuddy.net>
2026-01-09 17:54:27 +0530

    feat: completed implementing utilities

--- core_utils/tempmain.py
+++ core_utils/tempmain.py
@@ -1,4 +1,15 @@
 from logger import Logger
+from config import Config
+# from retry import Retry
+from rate_limiter import RateLimiter
+# logs=Logger("INFO","core_utils/logs.txt")
+# logs.error("debug")
 
-a=Logger("INFO",)
+# configs=Config("core_utils/config.json")
+# print(configs.get("HOME"))
 
+rl=RateLimiter(2,5)
+rl.allow()
+rl.allow()
+rl.allow()
+rl.allow()
